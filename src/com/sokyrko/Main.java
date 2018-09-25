6a681aa03ee2a59c251a9faab1744ce4d4f750ad
marilov <sokyrko.marina@gmail.com>
2018-09-25 21:59:11 +0300

    Validation tasks

--- src/com/sokyrko/Main.java
+++ src/com/sokyrko/Main.java
@@ -1,121 +1,107 @@
 package com.sokyrko;
 
+import java.util.ArrayList;
+
 public class Main {
 
     public static void main(String[] args) {
-
-        int[] arrayNumbers = {1, 2, 3, 4, 5, 6, 7, 8, 9};
-        int[] arrayCopy = arrayNumbers.clone();
-        int[] arrayNumbers2 = {1, 2, 3, 4, 5, 6, 7, 8, 9};
-
-        findRepeatedNumbers(arrayNumbers);
-        findSum(arrayNumbers);
-        deleteElement(arrayNumbers);
-        findAverage(arrayNumbers);
-        diagonalElements(arrayNumbers);
-        copyArray(arrayNumbers, arrayCopy);
-        sumArrays(arrayNumbers, arrayNumbers2);
-
-    }
-
-    //Task_1: Program finds array sum
-
-    public static int findSum(int[] arrayNumbers) {
-        int sum = 0;
-        for (int i = 0; i < arrayNumbers.length; i++) {
-            sum = sum + arrayNumbers[i];
-        }
-        return sum;
-    }
-
-    //Task_2: Program deletes preset element from array
-
-    public static void deleteElement(int[] arrayNumbers) {
-        int deletedNumber = 3;
-        int i = 0;
-        boolean isPresent = false;
-        int arraySize = arrayNumbers.length;
-
-        for (i = 0; i < arraySize; i++) {
-            if (arrayNumbers[i] == deletedNumber) {
-                isPresent = true;
-                break;
-            }
-        }
-        if (isPresent) {
-            for (int j = i; j < arraySize - 1; j++) {
-                arrayNumbers[j] = arrayNumbers[j + 1];
-            }
-            arraySize--;
-
-            for (i = 0; i < arraySize; i++) {
-                System.out.print(arrayNumbers[i] + " ");
-                System.out.println();
-            }
-        } else {
-            System.out.println("Нечего удалять");
-        }
-    }
-
-
-    //Task_3: Program finds repeated numbers in the array
-
-    public static void findRepeatedNumbers(int[] arrayNumbers) {
-        int sum = 0;
-
-        for (int i = 0; i < arrayNumbers.length; i++) {
-            for (int j = i + 1; j < arrayNumbers.length; j++)
-                if (arrayNumbers[i] == arrayNumbers[j]) {
-                    System.out.println("Все повторяющиеся значения: " + arrayNumbers[j]);
-                    if (arrayNumbers[j] != arrayNumbers[i]) {
-                        System.out.println("Повторяющихся значений нет.");
-                    }
-                }
-        }
-    }
-
-    //Task_4: Program finds average number in the array
-
-    public static void findAverage(int[] arrayNumbers) {
-        double average = 0;
-        for (int i = 0; i < arrayNumbers.length; i++) {
-            average = findSum(arrayNumbers) / arrayNumbers.length;
-        }
-        System.out.println("Среднее арифметическое: " + average);
-    }
-
-
-    //Task_5: Program shows all array elements diagonally
-
-    public static void diagonalElements(int[] arrayNumbers) {
-        outer:
-        for (int i = 0; i < arrayNumbers.length; i++) {
-            for (int j = 0; j < arrayNumbers.length; j++) {
-                if (j > i) {
-                    System.out.println();
-                    continue outer;
-                }
-                System.out.print(" " + arrayNumbers[j]);
-            }
-        }
-    }
-    //Task_6: Program clones array
-
-    public static void copyArray(int[] arrayNumbers, int[] arrayCopy) {
-        for (int i = 0; i < arrayCopy.length; i++) {
-            System.out.println("\n" + "Новый массив: " + arrayCopy[i] + " ");
-        }
-    }
-
-    //Task_6: Program sums arrays
-
-    public static void sumArrays(int[] arrayNumbers, int[] arrayNumbers2) {
-        int[] arraySum = new int[arrayNumbers2.length];
-
-        for (int i = 0; i < arrayNumbers2.length; i++) {
-                arraySum[i] = arrayNumbers2[i] + arrayNumbers[i];
-                System.out.println("\n" + "Сумма двух массивов: " + arraySum[i] + " ");
-        }
     }
 }
+
+//Validate {1,2} array
+
+
+class TestTaskAll {
+
+    public ArrayList<String> format(ArrayList<String> src) {
+
+        //new array
+        ArrayList<String> formatted = new ArrayList<>();
+
+        //loop
+        for (String currentElement : src) {
+            //check: isValid
+            if (isValid(currentElement)) {
+                formatted.add(currentElement);
+            }
+        }
+        return formatted;
+    }
+
+    private Boolean isValid(String element) {
+
+
+        if (element.startsWith("{") && element.endsWith("}")) {
+            element = element.substring(1, element.length() - 1);
+            String[] numbers = element.split(",");
+            if (numbers.length != 2) {
+                return false;
+            }
+
+            int left = Integer.parseInt(numbers[0]);
+            int right = Integer.parseInt(numbers[1]);
+
+            return left < right;
+
+        }
+        return false;
+    }
+}
+
+//Remove spaces from sentence
+
+class SentenceTask {
+
+    public static void main(String[] args) {
+
+        int wordNumber = countWords("Hello, this is an interesting functions, parse it");
+        System.out.println(wordNumber);
+
+    }
+
+    public static int countWords(String sentence) {
+
+        String[] words = sentence.split(" ");
+
+        return words.length;
+    }
+}
+
+// Validate phone number
+
+class NumbersTask {
+
+    public static void main(String[] args) {
+        System.out.println(validFormat("+380935202020"));
+    }
+
+    public static Boolean validFormat(String number) {
+
+        boolean isContainFormat = false;
+        boolean isNumberSize = false;
+        boolean isWithoutSpaces = false;
+        boolean isWithoutChars = false;
+
+        if (number.startsWith("+380")) {
+            isContainFormat = true;
+        }
+
+        if (number.length() == 13) {
+            isNumberSize = true;
+        }
+
+        if (!number.contains(" ")) {
+            isWithoutSpaces = true;
+        }
+
+        if (number.substring(1).matches("[0-9]+")) {
+            isWithoutChars = true;
+        }
+
+        return isContainFormat & isNumberSize & isWithoutSpaces & isWithoutChars;
+    }
+
+}
+
+
 
