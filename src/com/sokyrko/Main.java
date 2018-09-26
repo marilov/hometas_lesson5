6c267f89682a9624b1b290e5cf944c3aa2a0a3dd
marilov <sokyrko.marina@gmail.com>
2018-09-26 14:23:37 +0300

    Validation tasks (delete all numbers from String)

--- src/com/sokyrko/Main.java
+++ src/com/sokyrko/Main.java
@@ -1,6 +1,8 @@
 package com.sokyrko;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 
 public class Main {
 
@@ -103,5 +105,74 @@ class NumbersTask {
 
 }
 
+//Checking if the word is Palindrome
 
+class PalindromeString {
 
+    public static void main(String[] args) {
+
+        System.out.println(isPalindrome("Madam"));
+    }
+
+    public static Boolean isPalindrome(String word) {
+        StringBuilder palindrome = new StringBuilder(word);
+        String str = palindrome.reverse().toString();
+        if (word.equals(str)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
+
+//Checking if two words are Anagramms
+
+class AnagrammClass {
+
+    public static void main(String[] args) {
+        System.out.println(isAnagramm("hEllo", "lohel"));
+    }
+
+    public static Boolean isAnagramm(String firstWord, String secordWord) {
+
+        if (firstWord.length() != secordWord.length()) {
+            return false;
+        } else {
+
+            firstWord.toLowerCase();
+            secordWord.toLowerCase();
+
+            char[] char1 = firstWord.toCharArray();
+            char[] char2 = secordWord.toCharArray();
+
+            Arrays.sort(char1);
+            Arrays.sort(char2);
+
+            String firstString = new String(char1);
+            String secondString = new String(char2);
+
+            if (firstString.equals(secondString)) {
+                return true;
+            } else {
+                return false;
+            }
+
+        }
+    }
+}
+
+//Removing numbers from String 
+
+class DeleteNumbers {
+
+    static String str = new String("Hello, I am 24, I am from Ukraine");
+
+    public static void main(String[] args){
+
+        System.out.println(removeNumbers(str));
+    }
+
+    public static String removeNumbers(String args){
+        return str.replaceAll("[0-9]+","");
+    }
+}
